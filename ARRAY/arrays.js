f1e0ba3bc433db3da842521c9741f304c7ea2b77
Your Name <your_email@example.com>
2025-05-06 15:29:45 +0530

    array

---
+++ ARRAY/arrays.js
@@ -0,0 +1,75 @@
+// Arrays
+
+
+// const num = [0,1,2,3,4,5]
+// console.log(num);
+// console.log(typeof num);
+
+
+
+// const names = ["vaushnavi","sakshi", 1, "banana"]
+// console.log(names);
+// console.log(typeof names);
+
+
+
+// Arrays methods
+// const myArr = [0,1,2,3,4,5]
+// myArr.push(6)
+// myArr.pop(7)
+// console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+
+//Slice and Splice
+
+
+// console.log("A", myArr);
+
+// const myn1 = myArr.slice(1,3)
+// console.log(myn1);
+// console.log("B", myArr);
+
+
+// const myn2 = myArr.splice(1,3)
+// console.log(myn2);
+
+
+// const student_name =["vaishnavi","vaibhavi","devika","diksha","sakshi"]
+
+// const boyes_name =["rohan","nilesh","kunal"]
+// // 
+
+// const allstudent = student_name.concat(boyes_name)
+// console.log(student_name);
+
+
+// let score1= 100
+// let score2= 200
+// let score3= 300
+// console.log(Array.of(score1,score2,score3));
+
+
+
+// const student_name =["vaishnavi","sakshi","diksha"]
+// console.log(student_name);
+
+
+const myArr = new Array (1,2,3,4)
+console.log(myArr[5]);
+
+// Array methods
+// myArr.push(6)
+// myArr.pop(4)
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr);
